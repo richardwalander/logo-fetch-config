297e9c45c47753f90eea75b43cdff44ebb17898e
Richard Wålander <richard.walander@gmail.com>
2024-09-13 16:08:47 +0200

    Added more categories

--- config.js
+++ config.js
@@ -177,6 +177,241 @@ var config = {
         },
       ],
     },
+    {
+      id: 'food',
+      icon: '🍣',
+      name: 'Food',
+      brands: [
+        {
+          name: 'McDonald',
+          domain: 'mcdonalds.com',
+          logo_url: 'https://img.logo.dev/mcdonalds.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'BURGER KING® EMEA',
+          domain: 'burgerking.com',
+          logo_url: 'https://img.logo.dev/burgerking.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Pizza Hut',
+          domain: 'pizzahut.co.uk',
+          logo_url: 'https://img.logo.dev/pizzahut.co.uk?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Subway Brasil',
+          domain: 'subway.com.br',
+          logo_url: 'https://img.logo.dev/subway.com.br?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Cola Company',
+          domain: 'coca-colacompany.com',
+          logo_url: 'https://img.logo.dev/coca-colacompany.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Pepsi',
+          domain: 'pepsi.com',
+          logo_url: 'https://img.logo.dev/pepsi.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Unleashing Potential',
+          domain: 'kelloggs.com',
+          logo_url: 'https://img.logo.dev/kelloggs.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Heinz',
+          domain: 'heinz.com',
+          logo_url: 'https://img.logo.dev/heinz.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+      ],
+    },
+    {
+      id: 'pharma',
+      icon: '💊',
+      name: 'Pharma',
+      brands: [
+        {
+          name: 'Vertex',
+          domain: 'vrtx.com',
+          logo_url: 'https://img.logo.dev/vrtx.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Regeneron Pharmaceuticals',
+          domain: 'regeneron.com',
+          logo_url: 'https://img.logo.dev/regeneron.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Johnson & Johnson: Changing health for humanity',
+          domain: 'jnj.com',
+          logo_url: 'https://img.logo.dev/jnj.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Lilly',
+          domain: 'lilly.com',
+          logo_url: 'https://img.logo.dev/lilly.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'AbbVie',
+          domain: 'abbvie.com',
+          logo_url: 'https://img.logo.dev/abbvie.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'GSK',
+          domain: 'gsk.com',
+          logo_url: 'https://img.logo.dev/gsk.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Merck',
+          domain: 'merck.com',
+          logo_url: 'https://img.logo.dev/merck.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+      ],
+    },
+    {
+      id: 'clothing',
+      icon: '👚',
+      name: 'Clothing',
+      brands: [
+        {
+          name: 'COACH UK Official Site',
+          domain: 'uk.coach.com',
+          logo_url: 'https://img.logo.dev/uk.coach.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Michael Kors',
+          domain: 'michaelkors.com',
+          logo_url: 'https://img.logo.dev/michaelkors.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Gap',
+          domain: 'gap.com',
+          logo_url: 'https://img.logo.dev/gap.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'PUMA',
+          domain: 'puma.com',
+          logo_url: 'https://img.logo.dev/puma.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Nike',
+          domain: 'nike.com',
+          logo_url: 'https://img.logo.dev/nike.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Denim & Styles',
+          domain: 'ralphlauren.co.uk',
+          logo_url: 'https://img.logo.dev/ralphlauren.co.uk?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Vans®',
+          domain: 'vans.com',
+          logo_url: 'https://img.logo.dev/vans.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+      ],
+    },
+    {
+      id: 'transport',
+      icon: '🚕',
+      name: 'Transport',
+      brands: [
+        {
+          name: 'Olacabs.com',
+          domain: 'olacabs.com',
+          logo_url: 'https://img.logo.dev/olacabs.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Tesla',
+          domain: 'tesla.com',
+          logo_url: 'https://img.logo.dev/tesla.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'BYD',
+          domain: 'byd.cn',
+          logo_url: 'https://img.logo.dev/byd.cn?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'UPS',
+          domain: 'ups.com',
+          logo_url: 'https://img.logo.dev/ups.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'DHL Home',
+          domain: 'dhl.com',
+          logo_url: 'https://img.logo.dev/dhl.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Hertz',
+          domain: 'hertz.com',
+          logo_url: 'https://img.logo.dev/hertz.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Uber United States',
+          domain: 'uber.com',
+          logo_url: 'https://img.logo.dev/uber.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Toyota Official Site',
+          domain: 'toyota.com',
+          logo_url: 'https://img.logo.dev/toyota.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'BMW',
+          domain: 'bmw.com',
+          logo_url: 'https://img.logo.dev/bmw.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+      ],
+    },
+    {
+      id: 'design',
+      icon: '🎨',
+      name: 'Design',
+      brands: [
+        {
+          name: 'Figma',
+          domain: 'figma.com',
+          logo_url: 'https://img.logo.dev/figma.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Adobe',
+          domain: 'adobe.com',
+          logo_url: 'https://img.logo.dev/adobe.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Behance',
+          domain: 'behance.net',
+          logo_url: 'https://img.logo.dev/behance.net?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Dribbble',
+          domain: 'dribbble.com',
+          logo_url: 'https://img.logo.dev/dribbble.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Framer',
+          domain: 'framer.com',
+          logo_url: 'https://img.logo.dev/framer.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Sketch · Design',
+          domain: 'sketch.com',
+          logo_url: 'https://img.logo.dev/sketch.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Inkscape',
+          domain: 'inkscape.org',
+          logo_url: 'https://img.logo.dev/inkscape.org?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'blender',
+          domain: 'blender.org',
+          logo_url: 'https://img.logo.dev/blender.org?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+        {
+          name: 'Creative Software For Professionals | Get 50% Off Today',
+          domain: 'affinity.serif.com',
+          logo_url: 'https://img.logo.dev/affinity.serif.com?token=pk_e1ID1mORQsmubAfDqApj-w',
+        },
+      ],
+    },
   ],
 }
 
